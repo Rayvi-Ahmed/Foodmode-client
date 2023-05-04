a878e3088e8e1fadd4c3609c7a655a00ce9fb3c9
Rayvi-Ahmed <rayvi73@gmail.com>
2023-05-04 16:36:20 +0600

    add recipe details

--- src/Pages/Recipe/Recipe.jsx
+++ src/Pages/Recipe/Recipe.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import EachReacipe from '../EachRecipe/EachReacipe';
 
 const Recipe = ({recipes}) => {
 
-    const {name,picture,recipe}=recipes
+    const {name,picture,recipe,bio,likes,ratings,num_of_recipes,years_of_experience}=recipes
     console.log(recipe)
     return (
-        <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src={picture} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+        <div className='container mx-auto p-5'>
+            <div className="bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+    <img src={picture} className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-3xl font-bold">{name}</h1>
+      <p className="py-6"><span className='font-bold'>Chef Bio:</span> {bio}.</p>
+      <h6 className='text-bg-blue-300 font-bold text-l'>Likes : {likes}</h6>
+      <h6 className='text-bg-blue-300 font-bold text-l'>Total experience :{years_of_experience} years</h6>
+      <h6 className='text-bg-blue-300 font-bold text-l'>Total Recipes :{num_of_recipes} Items</h6>
+      <h6 className='text-bg-blue-300 font-bold text-l'>Total Recipes :{ratings} star</h6>
+    
     </div>
+    
   </div>
+
+ 
+</div>
+<div className='my-5 flex justify-center items-center gap-6'>
+{
+  recipe.map(rcp=><EachReacipe
+  key={rcp.id}
+  rcp={rcp}
+  
+  >
+
+  </EachReacipe>)
+}
+
 </div>
         </div>
     );
