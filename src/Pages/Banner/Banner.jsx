b94a6acc2bf10727b21783e34d67ecb725f2f77b
Rayvi-Ahmed <rayvi73@gmail.com>
2023-05-05 12:40:54 +0600

    added banner about blog

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
     <img className='w-3/4' src={bannerImg} alt=''/>
     <div>
       <h1 className="text-5xl font-bold">Welcome to FoodMood!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <Link to='/blog' className="btn btn-primary">About Us</Link>
+      <p className="py-6">This is the italian biggest food recipe website, here you can know about many of world class italian chefs information with many of delecius food EachReacipe.</p>
+      <Link to='/about' className="btn btn-primary">About Us</Link>
     </div>
   </div>
 </div>
